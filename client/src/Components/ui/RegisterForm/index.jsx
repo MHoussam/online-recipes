3d9a2c9d034724e893e14eea6feaae2fcff0a9a4
MHoussam <mohammadosta1999@gmail.com>
2023-08-24 01:45:14 +0300

    Client <>Created the Login Page & the Navbar in the Home Page.

--- client/src/Components/ui/RegisterForm/index.jsx
+++ client/src/Components/ui/RegisterForm/index.jsx
@@ -3,80 +3,107 @@ import axios from "axios";
 import "../../../styles/loginForm.css";
 import "../../../styles/utilities.css";
 import { useNavigate } from "react-router-dom";
-
-const RegisterForm = ({setUser}) =>{
+import Image from "../../base/Image";
+import logoImage from "../../../assets/images/tastybites.png";
+import Button from "../../base/Button";
+const RegisterForm = ({ setUser }) => {
   const [data, setData] = useState({
-    name: '',
-    username: '',
-    email: '',
-    password: ''
-  })
+    fname: "",
+    lname: "",
+    email: "",
+    password: "",
+  });
 
-  const handleDataChange = (e)=>{
-    setData({...data, [e.target.name]: e.target.value})
-  }
+  const handleDataChange = (e) => {
+    setData({ ...data, [e.target.name]: e.target.value });
+  };
 
-  const handleSubmit = async ()=>{
-    try{
-      const response = await axios.post("http://localhost:8000/api/register", data)
-      console.log(response.data)
-      localStorage.setItem('id', response.data.id)
-      localStorage.setItem('name', response.data.name)
-      localStorage.setItem('token', response.data.token)
-      setData({name:'', username: '', email: '', password: ''})
-      navigate("/");
-    }catch(e){
-      console.log(e)
+  const handleSubmit = async () => {
+    try {
+      const response = await axios.post("http://localhost:8000/api/register", data);
+      console.log(response.data);
+      localStorage.setItem("id", response.data.id);
+      localStorage.setItem("first_name", response.data.name);
+      localStorage.setItem("token", response.data.token);
+      setData({ fname: "", lname: "", email: "", password: "" });
+
+      if (response.data.token !== null) {
+        navigate("/Home");
+      }
+    } catch (e) {
+      console.log(e);
     }
-  }
+  };
 
-  const handleLogin = ()=>{
+  const handleLogin = () => {
     navigate("/");
-  }
+  };
 
   const navigate = useNavigate();
 
   return (
-    <>
-      <div className="addForm flex center">
-        <div className="contactForm_container flex column">
-        <div className="names column flex center">
-            <div className="fname flex column width-50">
-              <label for="first_name"><span className="bold">Name:</span></label>
-              <input name="name" className="first_name padding" placeholder="Name" value={data.first_name} onChange={handleDataChange}/>
-            </div>
-            <div className="lname flex column width-50">
-              <label for="last_name"><span className="bold">Username</span></label>
-              <input name="username" className="padding" placeholder="Username" value={data.last_name} onChange={handleDataChange}/>
-            </div>
+    <div>
+      <div className="login-container">
+        <div className="form-container">
+        <div className="logo flex center">
+          <Image src={logoImage} alt="Tasty Bites Logo"className="logoPhoto flex column center" />
+        </div>
+          <h2>Register</h2>
+          <div className="input">
+            <label>First Name:</label>
+            <input
+              type="text"
+              name="first_name"
+              placeholder="First Name"
+              value={data.name}
+              className="fname-input"
+              onChange={handleDataChange}
+            />
           </div>
-
-          <div className="names column flex center">
-            <div className="fname flex column width-50">
-              <label for="first_name"><span className="bold">Email:</span></label>
-              <input name="email" className="first_name padding" placeholder="Email" value={data.first_name} onChange={handleDataChange}/>
-            </div>
-            <div className="lname flex column width-50">
-              <label for="last_name"><span className="bold">Password</span></label>
-              <input name="password" className="padding" placeholder="Password" type="password" value={data.last_name} onChange={handleDataChange}/>
-            </div>
+          <div className="input">
+            <label>Last Name:</label>
+            <input
+              type="text"
+              name="last_name"
+              placeholder="Last Name"
+              value={data.username}
+              className="lname-input"
+              onChange={handleDataChange}
+            />
           </div>
-          
-          <div className="btn flex column">
-            <div className="flex center">
-              <button className="add-contact-btn bold pointer" onClick={handleSubmit}>Register</button>
-            </div>
-
-            <br/>
-            
-            <div className="flex center">
-              <div>Already have an account? <a className="login-link" onClick={handleLogin}>Login</a></div>
-            </div>
+          <div className="input">
+            <label>Email:</label>
+            <input
+              type="email"
+              name="email"
+              placeholder="Email"
+              value={data.email}
+              className="email-input"
+              onChange={handleDataChange}
+            />
+          </div>
+          <div className="input">
+            <label>Password:</label>
+            <input
+              type="password"
+              name="password"
+              placeholder="Password"
+              value={data.password}
+              className="password-input"
+              onChange={handleDataChange}
+            />
+          </div>
+          <div className="login">
+            <Button text={'Register'} className="login-btn bold" onClick={handleSubmit} />
+          </div>
+          <div className="register-link">
+            Already have an account?
+            <a onClick={handleLogin}> Login</a>
           </div>
         </div>
       </div>
-  </>
-  )
-}
+    </div>
+  );
+};
 
 export default RegisterForm;
